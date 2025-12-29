269fb020b9a5a272ff9a7f0e30de3e19c8d9ea47
k-valchuk <valchuk.kirya@yandex.ru>
2025-12-29 16:10:38 +0300

    small changes

--- src/LampIndicator.h
+++ src/LampIndicator.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <QtWidgets>
 
-constexpr QColor RED = QColor(231, 76, 60);
-constexpr QColor GREEN = QColor(46, 204, 113);
-constexpr QColor GRAY = QColor(160, 160, 160);
+const QColor RED = QColor(231, 76, 60);
+const QColor GREEN = QColor(46, 204, 113);
+const QColor GRAY = QColor(160, 160, 160);
 
 class LampIndicator: public QWidget {
     Q_OBJECT
